f042f32314dd9d22d112e8bb1d9d1e089061d54f
Glenn Spinner <glenngineer1@gmail.com>
2016-05-02 09:07:53 -0500

    working on delete function, not quite there yet

--- xhr.js
+++ xhr.js
@@ -9,20 +9,31 @@ function executeThisCodeAfterFileIsLoaded () {
 var data = JSON.parse(this.responseText);
 // console.log("data", data);
 
-
 var songList = document.getElementById("songs");
+
+songList.addEventListener("click", removeElement);
+
+function removeElement(event) {
+    // Removes an element from the document
+    // var element = document.getElementById(songList);
+    event.target.remove();
+    console.log("",event);
+}
 
 for( currentSong in data.songs) {
   var songData = "";
   var song = data.songs[currentSong];
   songData += "<div class='title-song'>Song Title: ";
   songData += song.title;
+  songData += "<input type='button' id='deleteButton' value='delete'></input>"
   songData += "</div>";
   songData += "<div class='artist-song'>Performed by: ";
   songData += song.artist;
+  songData += "<input type='button' id='deleteButton' value='delete'></input>"
   songData += "</div>";
   songData += "<div class='album-song'>On the album: ";
   songData += song.album;
+  songData += "<input type='button' id='deleteButton' value='delete'></input>"
   songData += "</div>";
 
   songList.innerHTML += songData;
@@ -37,3 +48,6 @@ myRequest.addEventListener("error", executeThisCodeIfXHRFails);
 myRequest.open("get", "songs.json");
 
 myRequest.send();
+
+
+
