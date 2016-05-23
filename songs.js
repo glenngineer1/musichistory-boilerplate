33ac1c384175f2474b20adf9153d73b1387ce057
Glenn Spinner <glenngineer1@gmail.com>
2016-05-23 11:55:00 -0500

    trying yet again

--- songs.js
+++ songs.js
@@ -8,7 +8,7 @@ var songsToDom = document.getElementById("song")
 songs.unshift("Unchained by Van Halen on the album Fair Warnning");
 songs.push("Don't You Give Up On Me by Lissie on the album My Wild West");
 songs.unshift("Cry Ophelia by Adam Cohen on the album Adam Cohen")
-songs.push("One Night Love Affair by Bryan Adams on the album Reckless")
+songs.push("One Night Love Affair by Bryan Adams on the album Reckless ")
 
 // Running loop to go through songs list
 for (var i = 0; i < songs.length; i++) {
