a15701bfad46b10d1485e798bb9f1a0959acde39
Glenn Spinner <glenngineer1@gmail.com>
2016-05-23 11:53:37 -0500

    trying this again

---
+++ songs.js
@@ -0,0 +1,19 @@
+// Declaring Array for Songs
+var songs = [];
+
+// Declaring variable for get element by ID
+var songsToDom = document.getElementById("song")
+
+// Adding songs to the array, unshift is front, push is end
+songs.unshift("Unchained by Van Halen on the album Fair Warnning");
+songs.push("Don't You Give Up On Me by Lissie on the album My Wild West");
+songs.unshift("Cry Ophelia by Adam Cohen on the album Adam Cohen")
+songs.push("One Night Love Affair by Bryan Adams on the album Reckless" )
+
+// Running loop to go through songs list
+for (var i = 0; i < songs.length; i++) {
+  songs[i]
+// Sending songs back to DOM
+  songsToDom.innerHTML += `<div>${songs[i]}</div>`;
+}
+
